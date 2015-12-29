0c3b081137f61d5e8e656b1e1a819cea43612219
Pavel Mazurin <kovpas@gmail.com>
2015-12-29 11:00:50 +0100

    Fix text runner

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -4,16 +4,16 @@ result=0
 
 for f in tests/*/; do
 	string="Test ${f%?}"
-	result="$(./${f}test.swift)"
+	test_result="$(./${f}test.swift)"
 	expected="$(<${f}result.txt)"
-	if [ "${result##*( )}" == "$expected" ]; then
+	if [ "${test_result}" == "$expected" ]; then
 		echo "\033[0;32m✓ $string\033[0m"
 	else
 		echo "\033[0;31m✗ $string\033[0m"
 		echo "Expected:"
 		echo $expected
 		echo "Actual result:"
-		echo $result
+		echo $test_result
 		result=1
 	fi
 done
